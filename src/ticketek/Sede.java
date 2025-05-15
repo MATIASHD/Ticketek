f33a8258e657d5336b6e697d7678d8e1ca2912bc
Cristian <47560615+turbina31@users.noreply.github.com>
2025-05-15 13:20:48 -0300

    Update Sede.java

--- src/ticketek/Sede.java
+++ src/ticketek/Sede.java
@@ -7,6 +7,16 @@ public abstract class Sede {
 	private int sector;
 	
 	public Sede(String nombre, String direccion, int capacidadMaxima, int sector) {
+		// Validar datos
+        if (nombre == null || nombre.isEmpty()) {
+            throw new RuntimeException("El nombre no puede ser nulo o vacío");
+        }
+        if (direccion == null || direccion.isEmpty()) {
+            throw new RuntimeException("La dirección no puede ser nula o vacía");
+        }
+        if (capacidadMaxima <= 0) {
+            throw new RuntimeException("La capacidad máxima debe ser positiva");
+	}
 		this.nombre = nombre;
 		this.direccion = direccion;
 		this.capacidadMaxima = capacidadMaxima;

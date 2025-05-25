a961ed8d62184a636dd3629a3316df58c2c6c963
Cristian <47560615+turbina31@users.noreply.github.com>
2025-05-25 19:48:15 -0300

    Update Sector.java

--- src/ticketek/Sector.java
+++ src/ticketek/Sector.java
@@ -1,7 +1,9 @@
 package ticketek;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Sector {
 	private String nombre;
@@ -9,11 +11,20 @@ public class Sector {
 	private List<Integer> asientosOcupados;
 	private double porcentajeAdicional;
 	
+	private Set<Integer> asientosDisponibles;
+	private int entradasVendidas;
+	
 	//Estadio/miniestadio
 	public Sector(String nombre, int capacidadMaxima) {
 		this.nombre = nombre;
 		this.capacidadMaxima = capacidadMaxima;
 		this.asientosOcupados = new ArrayList<>();
+		
+		this.asientosDisponibles = new HashSet<>();
+        for (int i = 1; i <= capacidadMaxima; i++) {
+            asientosDisponibles.add(i);
+        }
+        this.entradasVendidas = 0;
 	}
 	
 	//para teatros
@@ -24,8 +35,17 @@ public class Sector {
 		this.porcentajeAdicional = porcentajeAdicional;
 	}
 	
+	public void reservarAsiento(int asiento) {
+        if (!asientosDisponibles.remove(asiento)) {
+            throw new IllegalArgumentException("Asiento no disponible");
+        }
+        entradasVendidas++;
+    }
+	
 	public int CantidadAsientosDisponibles() {
 		return this.capacidadMaxima - this.asientosOcupados.size();
 	}
-	
+	public Set<Integer> getAsientosDisponibles() {
+        return asientosDisponibles;
+    }
 }

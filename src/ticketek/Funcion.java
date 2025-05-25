4d6b499857e7fcd32d1e42087c69ffbc143b2232
Cristian <47560615+turbina31@users.noreply.github.com>
2025-05-25 19:46:48 -0300

    Update Funcion.java

--- src/ticketek/Funcion.java
+++ src/ticketek/Funcion.java
@@ -2,21 +2,28 @@ package ticketek;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class Funcion {
 	private String fecha;
-	private String sede;
+	private Sede sede;
 	private double precioBase;
 	private Espectaculo espectaculo;
 	private HashMap<String, Sector> sectores;
+	private int entradasVendidas;
+	private List<IEntrada> entradaList;
 	
-	
-	public Funcion(String fecha, String sede, double precioBase) {
+	public Funcion(String fecha, Sede sede, double precioBase) {
 		this.sede = sede;
 		this.fecha = fecha;
 		this.precioBase = precioBase;
 		this.sectores = new HashMap<String, Sector>();
+		this.entradasVendidas = 0;
+		this.entradaList = new ArrayList<>();
 	}
 	
 	public boolean venderEntrada(String sector, int fila, int asiento) {
@@ -36,6 +43,10 @@ public class Funcion {
 		return this.sede;
 	}
 	
+	public int getEntradasVendidas() {
+        return entradasVendidas;
+    }
+	
 	public int compararDate(String fecha1, String fecha2) {  
 	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");  
 	    LocalDate date1 = LocalDate.parse(fecha1, formatter);  
@@ -46,5 +57,21 @@ public class Funcion {
 		// resultado < 0 → fecha1 es anterior  
 		// resultado == 0 → iguales  
 		// resultado > 0 → fecha1 es posterior 
-	}  
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public Sede getSede() {
+		return sede;
+	}
+
+	public double getPrecioBase() {
+		return precioBase;
+	}
+	
+	public List<IEntrada> getEntradaList() {
+        return entradaList;
+    }
 }

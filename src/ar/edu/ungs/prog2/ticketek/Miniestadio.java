8994cd811d3ac31747e376893c988a62967dad3f
Cristian <47560615+turbina31@users.noreply.github.com>
2025-06-06 16:14:41 -0300

    Update Miniestadio.java

--- src/ar/edu/ungs/prog2/ticketek/Miniestadio.java
+++ src/ar/edu/ungs/prog2/ticketek/Miniestadio.java
@@ -1,66 +1,69 @@
 package ar.edu.ungs.prog2.ticketek;
 
 public class Miniestadio extends EstadiosConSecciones{
-		private int puestosDeComida;
-		private double AdicionalLibre;
+	private int cantidadPuestos;
+    private double precioConsumicion;
+    private Map<String, Sector> sectores;
+    private int asientosPorFila;
 
-	public Miniestadio(String nombre, String direccion, int capacidadMaxima, int asientoPorFila,int puestosDeComida, double adicionalLibre, String[] sector,
-			int[] capacidadSector, int[] porcentajeAdicional) {
-		super(nombre, direccion, capacidadMaxima, asientoPorFila, sector, capacidadSector, porcentajeAdicional);
-		this.puestosDeComida = puestosDeComida;
-		this.AdicionalLibre = adicionalLibre;
-		// TODO Auto-generated constructor stub
-	}
+    public Miniestadio(String nombre, String direccion, int capacidadMaxima, int asientosPorFila,
+                       int cantidadPuestos, double precioConsumicion, String[] nombresSectores,
+                       int[] capacidades, int[] porcentajes) {
+        super(nombre, direccion, capacidadMaxima);
+        if (asientosPorFila <= 0 || cantidadPuestos <= 0 || precioConsumicion < 0 ||
+                nombresSectores == null || capacidades == null || porcentajes == null ||
+                nombresSectores.length != capacidades.length || capacidades.length != porcentajes.length) {
+            throw new IllegalArgumentException("Datos de miniestadio no válidos");
+        }
+        this.cantidadPuestos = cantidadPuestos;
+        this.precioConsumicion = precioConsumicion;
+        this.asientosPorFila = asientosPorFila;
+        this.sectores = new HashMap<>();
+        int totalCapacidad = 0;
+        for (int i = 0; i < nombresSectores.length; i++) {
+            sectores.put(nombresSectores[i], new Sector(nombresSectores[i], capacidades[i], asientosPorFila, porcentajes[i]));
+            totalCapacidad += capacidades[i];
+        }
+        if (totalCapacidad > capacidadMaxima) {
+            throw new IllegalArgumentException("Capacidad de sectores excede capacidad máxima");
+        }
+    }
 
-	/*@Override
-	public void reservarAsiento(String sector, int asiento) {
-		// TODO Auto-generated method stub
-		
-	}
+    public boolean asignarAsiento(String sector, int asiento) {
+        Sector s = sectores.get(sector);
+        if (s == null) {
+            throw new IllegalArgumentException("Sector no existe");
+        }
+        return s.asignarAsiento(asiento);
+    }
 
-	@Override
-	public boolean esSectorValido(String sector) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public void liberarAsiento(String sector, int asiento) {
+        Sector s = sectores.get(sector);
+        if (s != null) {
+            s.liberarAsiento(asiento);
+        }
+    }
 
-	@Override
-	public Set<Integer> getAsientosDisponibles(String sector) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Sector getSector(String sector) {
+        return sectores.get(sector);
+    }
 
-	@Override
-	public void descontarAsiento(String sector, int[] asientos) {
-		// TODO Auto-generated method stub
-		super.descontarAsiento(sector, asientos);
-	}*/
-	
-	public double adicional() {
-		return this.AdicionalLibre;
-	}
-	public int puesto() {
-		return this.puestosDeComida;
-	}
+    public double getPrecioConsumicion() {
+        return precioConsumicion;
+    }
 
-	@Override
-	public void descontarAsiento(String sector, int[] asientos) {
-		// TODO Auto-generated method stub
-		super.descontarAsiento(sector, asientos);
-	}
+    @Override
+    public boolean esNumerada() {
+        return true;
+    }
 
-	@Override
-	public boolean compararSede(String sede) {
-		// TODO Auto-generated method stub
-		return super.compararSede(sede);
-	}
-
-	@Override
-	public int porcentajeRecargo(String sector) {
-		// TODO Auto-generated method stub
-		return super.porcentajeRecargo(sector);
-	}
-	
-	
-	
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(nombre + " (MiniEstadio, Capacidad: " + capacidadMaxima +
+                ", Puestos: " + cantidadPuestos + ", Sectores: ");
+        for (Sector s : sectores.values()) {
+            sb.append(s.getNombre()).append(", ");
+        }
+        return sb.toString().substring(0, sb.length() - 2) + ")";
+    }
 }

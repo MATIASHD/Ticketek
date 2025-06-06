29c01b55352b2b881f70d7fb5ac0a810cbadc392
Cristian <47560615+turbina31@users.noreply.github.com>
2025-06-06 16:29:40 -0300

    Update Teatro.java

--- src/ar/edu/ungs/prog2/ticketek/Teatro.java
+++ src/ar/edu/ungs/prog2/ticketek/Teatro.java
@@ -1,64 +1,58 @@
 package ar.edu.ungs.prog2.ticketek;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Teatro extends EstadiosConSecciones{
-	
-	public Teatro(String nombre, String direccion, int capacidadMaxima, int asientoPorFila, String[] sector,
-			int[] capacidadSector, int[] porcentajeAdicional) {
-		super(nombre, direccion, capacidadMaxima, asientoPorFila, sector, capacidadSector, porcentajeAdicional);
-		// TODO Auto-generated constructor stub
-	}
+	protected Map<String, Sector> sectores;
+    protected int asientosPorFila;
 
-	/*public double precioDeEntrada() {
-		return 1; //VER COMO SEGUIR
-	}
-	
-	public int getAsiento() {
-		return 1;
-	}*/
+    public Teatro(String nombre, String direccion, int capacidadMaxima, int asientosPorFila, String[] nombresSectores, int[] capacidades, int[] porcentajes) {
+        super(nombre, direccion, capacidadMaxima);
+        if (asientosPorFila <= 0 || nombresSectores == null || capacidades == null || porcentajes == null ||
+                nombresSectores.length != capacidades.length || capacidades.length != porcentajes.length) {
+            throw new RuntimeException("Datos de sectores no válidos");
+        }
+        this.asientosPorFila = asientosPorFila;
+        this.sectores = new HashMap<>();
+        int totalCapacidad = 0;
+        for (int i = 0; i < nombresSectores.length; i++) {
+            sectores.put(nombresSectores[i], new Sector(nombresSectores[i], capacidades[i], asientosPorFila, porcentajes[i]));
+            totalCapacidad += capacidades[i];
+        }
+        if (totalCapacidad > capacidadMaxima) {
+            throw new RuntimeException("Capacidad de sectores excede capacidad máxima");
+        }
+    }
 
-	/*@Override
-	public void reservarAsiento(String sector, int asiento) {
-		// TODO Auto-generated method stub
-		
-	}
+    public boolean asignarAsiento(String sector, int asiento) {
+        Sector s = sectores.get(sector);
+        if (s == null) {
+            throw new IllegalArgumentException("Sector no existe");
+        }
+        return s.asignarAsiento(asiento);
+    }
 
-	@Override
-	public boolean esSectorValido(String sector) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public void liberarAsiento(String sector, int asiento) {
+        Sector s = sectores.get(sector);
+        if (s != null) {
+            s.liberarAsiento(asiento);
+        }
+    }
 
-	@Override
-	public Set<Integer> getAsientosDisponibles(String sector) {
-		// TODO Auto-generated method stub
-		return null;
-	}*/
+    @Override
+    public boolean esNumerada() {
+        return true;
+    }
 
-	@Override
-	public void descontarAsiento(String sector, int[] asientos) {
-		// TODO Auto-generated method stub
-		super.descontarAsiento(sector, asientos);
-	}
+    public Sector getSector(String sector) {
+        return sectores.get(sector);
+    }
 
-	@Override
-	public int obtenerCapcidadMaxima() {
-		// TODO Auto-generated method stub
-		return super.obtenerCapcidadMaxima();
-	}
-
-	@Override
-	public int porcentajeRecargo(String sector) {
-		// TODO Auto-generated method stub
-		return super.porcentajeRecargo(sector);
-	}
-
-	@Override
-	public boolean compararSede(String sede) {
-		// TODO Auto-generated method stub
-		return super.compararSede(sede);
-	}
-	
-	
-
-
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(nombre + " (Teatro, Capacidad: " + capacidadMaxima + ", Sectores: ");
+        for (Sector s : sectores.values()) {
+            sb.append(s.getNombre()).append(", ");
+        }
+        return sb.toString().substring(0, sb.length() - 2) + ")";
+    }
 }

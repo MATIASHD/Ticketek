d325a4fe26cb60fb709d136b7dc0171675865627
Cristian <47560615+turbina31@users.noreply.github.com>
2025-06-06 16:22:59 -0300

    Update Sector.java

--- src/ar/edu/ungs/prog2/ticketek/Sector.java
+++ src/ar/edu/ungs/prog2/ticketek/Sector.java
@@ -5,33 +5,61 @@ import java.util.List;
 
 public class Sector {
 	private String nombre;
-	private int capacidadMaxima;
-	private List<Integer> asientosOcupados;
-	private double porcentajeAdicional;
-	
-	//Estadio/miniestadio
-	public Sector(String nombre, int capacidadMaxima) {
-		this.nombre = nombre;
-		this.capacidadMaxima = capacidadMaxima;
-		this.asientosOcupados = new ArrayList<>();
-	}
-	
-	public String obtenerNombre() {
-		return this.nombre;
-	}
-	public double obtenerAdicional() {
-		return this.porcentajeAdicional;
-	}
-	//para teatros
-	public Sector(String nombre, int capacidadMaxima, double porcentajeAdicional) {
-		this.nombre = nombre;
-		this.capacidadMaxima = capacidadMaxima;
-		this.asientosOcupados = new ArrayList<>();
-		this.porcentajeAdicional = porcentajeAdicional;
-	}
-	
-	public int CantidadAsientosDisponibles() {
-		return this.capacidadMaxima - this.asientosOcupados.size();
-	}
-	
+    private int capacidad;
+    private int asientosPorFila;
+    private int porcentajeAdicional;
+    private boolean[] asientosOcupados;
+
+    public Sector(String nombre, int capacidad, int asientosPorFila, int porcentajeAdicional) {
+        if (nombre == null || nombre.isEmpty() || capacidad <= 0 || asientosPorFila <= 0 || porcentajeAdicional < 0) {
+            throw new IllegalArgumentException("Datos de sector no válidos");
+        }
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.asientosPorFila = asientosPorFila;
+        this.porcentajeAdicional = porcentajeAdicional;
+        this.asientosOcupados = new boolean[capacidad];
+    }
+
+    public boolean asignarAsiento(int asiento) {
+        if (asiento < 0 || asiento >= capacidad || asientosOcupados[asiento]) {
+            return false;
+        }
+        asientosOcupados[asiento] = true;
+        return true;
+    }
+
+    public void liberarAsiento(int asiento) {
+        if (asiento >= 0 && asiento < capacidad) {
+            asientosOcupados[asiento] = false;
+        }
+    }
+
+    public int getEntradasVendidas() {
+        int count = 0;
+        for (boolean ocupado : asientosOcupados) {
+            if (ocupado) count++;
+        }
+        return count;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public int obtenerAdicional() {
+        return porcentajeAdicional;
+    }
+
+    public int getFila(int asiento) {
+        return asiento / asientosPorFila + 1;
+    }
+
+    public int getNumeroAsiento(int asiento) {
+        return asiento % asientosPorFila + 1;
+    }
 }

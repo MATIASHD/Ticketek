d3e1075b9b021a15779b1ef9ec08cc6e9d0be285
Cristian <47560615+turbina31@users.noreply.github.com>
2025-06-06 16:17:33 -0300

    Update Funcion.java

--- src/ar/edu/ungs/prog2/ticketek/Funcion.java
+++ src/ar/edu/ungs/prog2/ticketek/Funcion.java
@@ -2,6 +2,8 @@ package ar.edu.ungs.prog2.ticketek;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Funcion {
 	private String fecha;
